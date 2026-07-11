5ff45fa5a07ba451309105758d3f79237229e903
sir-shivem <sirshivam95@gmail.com>
2026-07-11 12:22:28 +0530

    changes in tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,24 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import AuthProvider from "@/components/providers/authProvider";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Billing - JustSimple",
+    template: "%s | JustSimple Billing",
+  },
+  description: "Simple and modern billing software for businesses.",
+
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      // { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      // { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    // apple: "/apple-icon.png",
+    shortcut: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
